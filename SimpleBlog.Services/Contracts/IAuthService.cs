ca662df5aa82b11167b234a8d726b5cbf1e7fa09
htphong24 <htphong24@gmail.com>
2022-02-14 21:08:47 -0800

    Added IAuthService

---
+++ SimpleBlog.Services/Contracts/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleBlog.Services.Contracts
+{
+    public interface IAuthService
+    {
+        string HashPassword(string password);
+        bool VerifyPassword(string actualPassword, string hashedPassword);
+        AuthData GetAuthData(string id);
+    }
+}
