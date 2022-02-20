4e4419489c670461a49266d412b97bc98e1ea78a
htphong24 <htphong24@gmail.com>
2022-02-19 23:27:21 -0800

    Moved AuthData to SimpleBlog.Data

--- SimpleBlog.Data/AuthData.cs
+++ SimpleBlog.Data/AuthData.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SimpleBlog.WebApp.ViewModels
+﻿namespace SimpleBlog.Data
 {
     public class AuthData
     {
