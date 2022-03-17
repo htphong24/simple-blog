0e854970afc52995e5bf996d298f3815ff7c1c63
htphong24 <htphong24@gmail.com>
2022-03-16 20:56:25 -0700

    Added IUserRepository

---
+++ SimpleBlog.Services/Contracts/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SimpleBlog.Data;
+using SimpleBlog.Data.Contracts;
+
+namespace SimpleBlog.Services.Contracts
+{
+    public interface IUserRepository : IEntityBaseRepository<User>
+    {
+        bool IsUsernameUniq(string username);
+        bool IsEmailUniq(string email);
+    }
+}
